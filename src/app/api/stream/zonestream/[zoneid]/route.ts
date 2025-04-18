40d9e40a9f4af5eaecf35665bb1270ccbf3cb9e4
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-04-18 20:09:31 +0530

    fix: correct parameter handling in zone stream route for improved clarity and consistency

--- src/app/api/stream/zonestream/[zoneid]/route.ts
+++ src/app/api/stream/zonestream/[zoneid]/route.ts
@@ -1,21 +1,19 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Stream from "../../../../../../models/stream.model";
-import { NextRequest } from "next/server";
 
 export async function POST(
   request: NextRequest,
-  { params }: { params: { zoneid: string } } // <-- correctly use `context`
+  context: { params: { zoneid: string } }
 ) {
-  const theParams = params;
-  const zoneId=theParams.zoneid;
+  const { zoneid } = context.params;
 
-  if (!zoneId) {
+  if (!zoneid) {
     return NextResponse.json({ message: "zone id required!" }, { status: 404 });
   }
 
   try {
-    console.log("this is the zone:",zoneId);
-    const tracks = await Stream.find({ zone: zoneId });
+    console.log("this is the zone:", zoneid);
+    const tracks = await Stream.find({ zone: zoneid });
 
     if (!tracks || tracks.length === 0) {
       return NextResponse.json(
@@ -24,7 +22,7 @@ export async function POST(
       );
     }
 
-    return NextResponse.json({ streams:tracks }, { status: 200 });
+    return NextResponse.json({ streams: tracks }, { status: 200 });
   } catch (err) {
     return NextResponse.json(
       { message: `error occurred: ${err}` },
